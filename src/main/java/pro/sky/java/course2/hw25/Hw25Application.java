3989930d08f130b72b59beb9fe5fcf147db71e12
nohero_ <serega1552@yandex.ru>
2022-05-06 23:10:53 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hw25/Hw25Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw25Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hw25/Hw25ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
